8ca6179e61f4bc511a1c0a5419399197dceb3304
kutaui <114265523+kutaui@users.noreply.github.com>
2022-11-29 22:42:58 +0300

    js done

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-const notificationNumber = document.querySelector('.number')
-const markRead = document.querySelector('h5')
-const redDot = document.querySelectorAll('.new-notif')
+let markRead = document.querySelector('#mark')
+let redDot = document.querySelectorAll('.new-notif')
+let activeCard = document.getElementsByClassName('card')
+let notificationNumber = document.querySelector('.number')
+
+markRead.addEventListener('click', () => {
+	;[...activeCard].forEach((element) => {
+		element.classList.remove('active')
+	})
+	;[...redDot].forEach((element) => {
+		element.classList.add('none')
+	})
+	notificationNumber.innerHTML = 0
+})
